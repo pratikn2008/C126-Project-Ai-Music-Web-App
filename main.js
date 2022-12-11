cd7340c4550f59b193e2fbfd210ecdbd6a8a4098
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-12-11 11:50:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 Peter_pan_song="";
 Harry_potter_theme_song="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup(){
     canvas = createCanvas(600,530);
@@ -7,6 +11,9 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function preload(){
@@ -16,4 +23,22 @@ function preload(){
 
 function draw(){
     image(video,0,0,600,530);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initiaized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = resulta[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console,log("leftWistX" + leftWristX + "leftWristY" + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX" + rightWristX + "rightWristY" + rightWristY);
+    }
 }
