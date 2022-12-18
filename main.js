3be661c47c20fc8c368bbe341aabcd05e73bfc39
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-12-18 10:55:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 Peter_pan_song="";
 Harry_potter_theme_song="";
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
 
 function setup(){
     canvas = createCanvas(600,530);
@@ -12,8 +14,8 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
 }
 
 function preload(){
@@ -23,22 +25,43 @@ function preload(){
 
 function draw(){
     image(video,0,0,600,530);
+
+    fill("#00ff00");
+    stroke("#ff0000");
+
+    song_name = Peter_pan_song.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        Harry_potter_theme_song.stop();
+        if(song_name == false){
+            Peter_pan_song.play();
+        }
+        else{
+            console.log("Song Name: Peter Pan Song");
+            document.getElementById("song_id").innerHTML = "Song Name: Peter Pan Song";
+        }
+    }
 }
 
 function modelLoaded(){
-    console.log("PoseNet Is Initiaized");
+    console.log("poseNet Is Initialized");
 }
 
-function gotPoses(results){
+function gotposes(results){
     if(results.length > 0){
         console.log(results);
 
-        leftWristX = resulta[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console,log("leftWistX" + leftWristX + "leftWristY" + leftWristY);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
 
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX" + rightWristX + "rightWristY" + rightWristY);
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
     }
 }
